a161c7b79abba88f9657c331bb128d78e3c26939
Oliveira <lucas.oliveira@cgi.com>
2023-02-22 16:51:45 +0000

    coçde correction

--- Library/src/main/java/pt/personal/Library/controller/Test.java
+++ Library/src/main/java/pt/personal/Library/controller/Test.java
@@ -1,18 +1,22 @@
 package pt.personal.Library.controller;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 
-@RestController("/lib")
+@RestController
+@RequestMapping("/api")
 public class Test {
 
     @GetMapping("/test")
-    public void test(){
+    public ResponseEntity test(){
+        return ResponseEntity.ok("Hello World");
     }
 }
